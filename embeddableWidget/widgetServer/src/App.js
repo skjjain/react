b3922e4fd5ed0cfd7096b86ac8fe82c1bc1dfc3d
Saurabh Jain <saurabh.jain@insurancedekho.com>
2021-11-01 15:29:17 +0530

    embedded widget added scss support

--- embeddableWidget/widgetServer/src/App.js
+++ embeddableWidget/widgetServer/src/App.js
@@ -1,4 +1,4 @@
-import './App.css'
-export default function App(){
-    return <div id="embeddedWidget">Hello World!!!</div>
+import "./App.scss";
+export default function App() {
+  return <div id="embeddedWidget"><h1>Hello World!!!</h1>This is an embeddable widget</div>;
 }
