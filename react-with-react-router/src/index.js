ee85731bf0a79851364e21a777702764da349814
Saurabh Jain <saurabh.jain@insurancedekho.com>
2021-11-05 19:31:25 +0530

    starting react-router-dom

---
+++ react-with-react-router/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+function component() {
+  const element = document.createElement("div");
+  element.id = "app-root";
+  return element;
+}
+document.body.appendChild(component());
+// document.getElementById("app-root").innerHTML = "Hello World!!!";
+ReactDom.hydrate(<div>Hello World!!!</div>, document.getElementById('app-root'))
